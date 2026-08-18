857b8e773ad7c76f6880a458a6430df550ab33b7
strangerintheq <tripmannn@gmail.com>
2026-08-18 13:37:03 +0300

    handle network settings save

--- src/modules/server/server.cpp
+++ src/modules/server/server.cpp
@@ -7,6 +7,7 @@
 #include <state.h>
 #include <utils/JsonBuilder.h>
 #include "ws.h"
+#include <ArduinoJson.h>
 
 AsyncWebServer server(80);
 
@@ -29,28 +30,40 @@ void startWebServer() {
       request->send(200, "application/json", b.c_str());
   });
 
+// Эндпоинт для сохранения настроек сети
+server.on("/network/set", HTTP_POST, 
+    [](AsyncWebServerRequest *request) {
+        request->send(200, "application/json", "{\"result\":\"ok\"}");
+    },
+    NULL, // upload handler (не нужен для json)
+    [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+        
+        JsonDocument doc;
+        DeserializationError error = deserializeJson(doc, data, len);
+        if (!error) {
+            if (doc["network_mode"].is<String>())   network_mode->write(doc["network_mode"].as<String>());
+            if (doc["wifi_ssid"].is<String>())     wifi_ssid->write(doc["wifi_ssid"].as<String>());
+            if (doc["wifi_password"].is<String>()) wifi_password->write(doc["wifi_password"].as<String>());
+            if (doc["ap_ssid"].is<String>())       ap_ssid->write(doc["ap_ssid"].as<String>());
+            if (doc["ap_password"].is<String>())   ap_password->write(doc["ap_password"].as<String>());
+            ESP.restart();
+        }
+    }
+);
+
+
   String pointsBin = "/points.bin";
 
   server.on("/upload", HTTP_POST, [pointsBin](AsyncWebServerRequest *request) {
       endWrite(pointsBin);
       initState();
-      request->send(200, "application/json", "{\"result\":\"saved_to_flash\"}");
+      request->send(200, "application/json", "{\"result\":\"ok\"}");
   }, NULL, [pointsBin](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
       if (index == 0) {
           startWrite(pointsBin);
       }
       writeBytes(pointsBin, data, len);
   });
-
-  // server.on("/status", HTTP_POST, [](AsyncWebServerRequest *request) {
-  //   // JsonBuilder<512> b;
-  //   // b.append("uptime", millis() );
-  //   // b.append("temp", getTemp());
-  //   // b.append("pointsCount", pointsCount);
-  //   // b.append("heapSize", ESP.getHeapSize());
-  //   // b.append("freeHeap", ESP.getFreeHeap());
-  //   request->send(200, "application/json", "{}");
-  // });
 
   server.on("/points", HTTP_POST, [](AsyncWebServerRequest *request) {
      request->send(LittleFS, "/points.bin", "application/octet-stream");
