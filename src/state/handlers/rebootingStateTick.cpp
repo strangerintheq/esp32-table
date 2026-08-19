60e641b54a40b0cd42cd1e4217b6e591ee9bb1e3
strangerintheq <tripmannn@gmail.com>
2026-08-19 23:53:31 +0300

    reboot

--- src/state/handlers/rebootingStateTick.cpp
+++ src/state/handlers/rebootingStateTick.cpp
@@ -1,4 +1,12 @@
+#include <Arduino.h>
+
+uint32_t rebootStartTimestamp = 0;
 
 void rebootingStateTick() {
-
+    uint32_t now = millis();
+    if (rebootStartTimestamp == 0)
+        rebootStartTimestamp = now;
+    if (now - rebootStartTimestamp < 500)
+        return;
+    ESP.restart();        
 }
